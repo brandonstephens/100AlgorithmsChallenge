3559817eb7c55bf5727caabed329c135feabdda1
Brandon Stephens <brandonstephens@me.com>
2020-04-14 09:41:05 -0400

    make it typescript

--- allLongestStrings/allLongestStrings.js
+++ allLongestStrings/allLongestStrings.js
@@ -1,12 +1,13 @@
-const allLongestStrings = (inputArray) => {
-  const longest = inputArray.map(x => x.length).sort((a,b) => b - a)[0]
-
-  return inputArray.reduce((acc,val) => {
-    if (val.length === longest) {
-      acc.push(val)
-    }
-    return acc
-  },[])
-}
-
+var allLongestStrings = function (inputArray) {
+    var longest = inputArray.map(function (x) { return x.length; }).sort(function (a, b) { return b - a; })[0];
+    return inputArray.reduce(function (acc, val) {
+        if (val.length === longest) {
+            acc.push(val);
+        }
+        return acc;
+    }, []);
+};
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "adddddd", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aaaa", "ddddx", "vcd", "aba"]));
+console.log(allLongestStrings(["a"]));
--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,14 @@
-function allLongestStrings(inputArray: string[]): string[] {
-
+const allLongestStrings = (inputArray: string[]): string[] => {
+  const longest: number = inputArray.map((x: string) => x.length).sort((a: number, b: number) => b - a)[0]
+  return inputArray.reduce((acc: string[], val: string) => {
+    if (val.length === longest) {
+      acc.push(val)
+    }
+    return acc
+  },[])
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "adddddd", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aaaa", "ddddx", "vcd", "aba"]));
+console.log(allLongestStrings(["a"]));
