715d9b4aab58ea4d4a4802ff4b9205b78f17de54
Brandon Stephens <brandonstephens@me.com>
2020-04-14 11:58:28 -0400

    less iteration

---
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -0,0 +1,35 @@
+var almostIncreasingSequence = function (sequence) {
+    //  const isIncreasing = (sequence: number[]): boolean => sequence.reduce((acc: boolean, val: number, index: number) => {
+    //    if(index !== sequence.length - 1) {
+    //      acc = sequence[index] < sequence[index + 1] ? true : false
+    //    }
+    //    return acc 
+    //  }, false)
+    //  
+    //  return sequence.reduce((acc: boolean, val: number, index: number) => {
+    //    if (acc === false) {
+    //      acc = isIncreasing(sequence.filter((x: number, i: number) => i !== index))
+    //    }
+    //    return acc
+    //  }, false)
+    var nonIncreasingCount = 0;
+    for (var i = 0; i < sequence.length; i++) {
+        if (sequence[i] <= sequence[i - 1]) {
+            nonIncreasingCount++;
+            if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+                return false;
+            }
+        }
+    }
+    return nonIncreasingCount <= 1;
+};
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+// false
+console.log(almostIncreasingSequence([1, 3, 1, 1]));
+// false
+console.log(almostIncreasingSequence([1, 3, 2]));
+// true
+console.log(almostIncreasingSequence([3, 3, 2, 1]));
+// false
+console.log(almostIncreasingSequence([2, 6, 100, 1]));
+// true
--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,7 +1,43 @@
 const almostIncreasingSequence = (sequence: number[]): boolean => {
+//  const isIncreasing = (sequence: number[]): boolean => sequence.reduce((acc: boolean, val: number, index: number) => {
+//    if(index !== sequence.length - 1) {
+//      acc = sequence[index] < sequence[index + 1] ? true : false
+//    }
+//    return acc 
+//  }, false)
+//  
+//  return sequence.reduce((acc: boolean, val: number, index: number) => {
+//    if (acc === false) {
+//      acc = isIncreasing(sequence.filter((x: number, i: number) => i !== index))
+//    }
+//    return acc
+//  }, false)
   
-  return true
+  let nonIncreasingCount = 0
+  
+  for(let i = 0; i < sequence.length; i++) {
+    if(sequence[i] <= sequence[i - 1]) {
+      nonIncreasingCount++
+      if(sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i-1]) {
+        return false
+      }
+    }
+  }
+  
+  return nonIncreasingCount <= 1
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])) // false
-console.log(almostIncreasingSequence([1, 3, 2])) // true
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+// false
+
+console.log(almostIncreasingSequence([1, 3, 1, 1])) 
+// false
+
+console.log(almostIncreasingSequence([1, 3, 2])) 
+// true
+
+console.log(almostIncreasingSequence([3, 3, 2, 1])) 
+// false
+
+console.log(almostIncreasingSequence([2, 6, 100, 1])) 
+// true
