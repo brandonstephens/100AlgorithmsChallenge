3b71a7de466f936d939ea6ad20383063abbd16b8
Brandon Stephens <brandonstephens@me.com>
2020-05-05 11:51:30 -0400

    did it

---
+++ alternatingSums/alternatingSums.js
@@ -0,0 +1,13 @@
+var alternatingSums = function (a) {
+    return a.reduce(function (acc, val, index) {
+        if (index % 2 !== 0) {
+            acc[1] += val;
+        }
+        else {
+            acc[0] += val;
+        }
+        return acc;
+    }, [0, 0]);
+};
+// [180, 105]
+console.log(alternatingSums([50, 60, 60, 45, 70]));
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,13 @@
-function alternatingSums(a: number[]): number[] {
-
+const alternatingSums = (a: number[]): number[] => {
+  return a.reduce((acc, val, index) => {
+    if(index % 2 !== 0) {
+      acc[1] += val
+    } else {
+      acc[0] += val
+    }
+    return acc
+  }, [0,0])
 }
 
+// [180, 105]
 console.log(alternatingSums([50, 60, 60, 45, 70]))
