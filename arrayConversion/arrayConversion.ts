8f1d6b19630ab292212489e7d43cbd1b771c08d9
Brandon Stephens <brandonstephens@me.com>
2020-05-08 11:43:29 -0400

    recusion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,25 @@
-function arrayConversion(inputArray: number[]): number {
+const arrayConversion = (inputArray: number[], pass: number = 1): number => {
+  if (inputArray.length === 1) {
+    return inputArray[0]
+  }
 
+  const outputArray = inputArray.reduce((acc, val, index) => {
+    if (index === 0 || index % 2 == 0) {
+      acc = (pass % 2) !== 0
+        ? [...acc, val + (inputArray[index + 1] || 0)]
+        : [...acc, val * (inputArray[index + 1] || 1)]
+    }
+    return acc
+  }, [])
+
+  return arrayConversion(outputArray, pass + 1)
 }
 
+// 186
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+
+// 16
+console.log(arrayConversion([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
+
+// 4
+console.log(arrayConversion([1, 1, 2]));
