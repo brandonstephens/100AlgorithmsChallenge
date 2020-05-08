fe2f22348555df217228158b58c906b7b6222113
Brandon Stephens <brandonstephens@me.com>
2020-05-07 20:56:22 -0400

    arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,26 @@
-function arrayChange(inputArray: number[]): number {
- 
+const arrayChange = (inputArray: number[]): number => {
+  return inputArray.reduce((acc, val, index) => {
+    if(index > 0) {
+      acc = {
+        prevValue: Math.abs(acc.prevValue) + 1,
+        count: acc.count + Math.abs(acc.prevValue - val) + 1
+      }
+    }
+    return acc
+  }, {prevValue: inputArray[0], count: 0}).count
 }
 
+// 3
 console.log(arrayChange([1, 1, 1]));
+
+// 6
+console.log(arrayChange([4, 2, 3]));
+
+// 10
+console.log(arrayChange([1, 1, 1, 1, 1]));
+
+//4
+console.log(arrayChange([3, 1, 4,]));
+
+
+
