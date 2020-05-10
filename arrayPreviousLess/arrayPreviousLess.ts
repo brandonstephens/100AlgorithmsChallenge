4fa2ada18464b75c6fc7aab2398fda61609ca779
Brandon Stephens <brandonstephens@me.com>
2020-05-10 18:14:18 -0400

    did it

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
-function arrayPreviousLess(items: number[]): number[] {
-
+const arrayPreviousLess = (items: number[]): number[] => {
+  return items.map((val, index) => {
+    return items.slice(0, index).reduce((acc, elm) => {
+      return elm < val
+        ? elm
+        : acc < elm
+          ? acc
+          : -1
+    }, -1)
+  })
 }
 
+// [-1, 3, -1, 2, 4]
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
